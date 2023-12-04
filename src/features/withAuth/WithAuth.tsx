82bc3b3295968122ed4f3048e186fe68f2addc0e
alex <o.barchuk@cthings.co>
2023-12-04 20:40:50 +0100

    add: example of WithAuth

--- src/features/withAuth/WithAuth.tsx
+++ src/features/withAuth/WithAuth.tsx
@@ -1,11 +1,19 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useState } from 'react';
 
 interface WithAuthProps {
   children: ReactNode;
 }
 
 export const WithAuth: FC<WithAuthProps> = ({ children }) => {
-  // some logic
+  // Here we will check the auth. The auth will be built with token and refresh token through API.
+
+  // Here we would normally make an API call to check the auth token
+  // For example: const auth = api.checkAuthToken();
+  // But for now, we will simulate this with a boolean
+  const [auth, setAuth] = useState(false);
+
+  // If the user is not authenticated, we could redirect them to a login page
+  // For example: if (!auth) return <Redirect to="/login" />
 
   return <>{children}</>;
 };
