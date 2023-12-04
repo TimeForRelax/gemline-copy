2f074c84cd98be3c3deb1608fd08f75b4e0ced6b
alex <o.barchuk@cthings.co>
2023-12-04 20:56:52 +0100

    add: useToken example of request

--- src/api/authentication/login.ts
+++ src/api/authentication/login.ts
@@ -1,1 +1,8 @@
-export default 15;
+import { HttpService } from '@utils/http';
+import { useQuery } from 'react-query';
+
+export const useApiToken = () => {
+  const { data } = useQuery(['Login'], {
+    queryFn: () => HttpService.get('some/url'),
+  });
+};
